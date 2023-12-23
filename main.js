d6be1a2b4da970ef7a9d2b500151c61f0ca27ca8
Anas Ramadan <99337703+anasramadan098@users.noreply.github.com>
2023-12-23 17:11:54 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+// Scroll
+
+// Get All Elements With Scroll Classes
+document.querySelectorAll('.slide-left,.slide-top,.slide-right,.slide-bottom').forEach(e => {
+    
+
+    // Remove All 
+
+    // Get It's Sreen Y 
+    const scrollElement = e.scrollHeight;
+
+    if (scrollY - 300 <= scrollElement) {
+        console.log('OK');
+    }
+    window.onscroll = function () {
+        if (scrollY - 300 <= scrollElement) {
+            console.log('OK');
+        }
+    }
+})
